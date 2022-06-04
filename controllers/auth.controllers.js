7a154889f3db46d96ea2f770dc063f1c7c8ed9df
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-04 15:01:32 +0430

    added jwt for login

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 const User = require("../models/user.model");
 const asyncWrapper = require("../middleware/async");
-const { createCustomError } = require("../error/custom-error");
+// const { createCustomError } = require("../error/custom-error");
 const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
 
 const userRegister = asyncWrapper(async (req, res, next) => {
     const newUser = new User({
@@ -16,6 +17,25 @@ const userRegister = asyncWrapper(async (req, res, next) => {
 
 const userLogin = asyncWrapper(async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
+
+    !user && res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+
+    const accessToken = JWT.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+        { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
 });
 
 module.exports = {
