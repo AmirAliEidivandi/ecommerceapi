00bc9c360fc1a9c6e04c03e19af83bf284c976c0
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-04 21:52:36 +0430

    completed users

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,20 +1,73 @@
 const asyncWrapper = require("../middleware/async");
 const { createCustomError } = require("../error/custom-error");
+const CryptoJS = require("crypto-js");
+const User = require("../models/user.model");
 
+// GET ALL USER
 const getAllUsers = asyncWrapper(async (req, res) => {
-    res.send("hello world");
+    const query = req.query.new;
+    const users = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find({});
+    res.status(200).json(users);
 });
 
-const getUser = asyncWrapper(async (req, res) => {});
+// GET USER
+const getUser = asyncWrapper(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+});
+
+// GET USER STATS
+const getUserStats = asyncWrapper(async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    const data = await User.aggregate([
+        { $match: { createdAt: { $gte: lastYear } } },
+        {
+            $project: {
+                month: { $month: "$createdAt" },
+            },
+        },
+        {
+            $group: {
+                _id: "$month",
+                total: { $sum: 1 },
+            },
+        },
+    ]);
+
+    res.status(200).json(data);
+});
 
 const postUser = asyncWrapper(async (req, res) => {
     const userName = req.body.username;
     res.send(userName);
 });
 
-const updateUser = asyncWrapper(async (req, res) => {});
+// UPDATE USER
+const updateUser = asyncWrapper(async (req, res, next) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+    }
 
-const deleteUser = asyncWrapper(async (req, res) => {});
+    const userUpdate = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set: req.body,
+        },
+        { new: true, runValidators: true }
+    );
+    res.status(200).json(userUpdate);
+});
+
+// DELETE USER
+const deleteUser = asyncWrapper(async (req, res, next) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    res.status(200).json("User has been deleted...");
+});
 
 module.exports = {
     getAllUsers,
@@ -22,4 +75,5 @@ module.exports = {
     postUser,
     updateUser,
     deleteUser,
+    getUserStats,
 };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
-const { getAllUsers, getUser, postUser, updateUser, deleteUser } = require("../controllers/users.controllers");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("../controllers/verifyToken");
+const { getUserStats, getAllUsers, getUser, postUser, updateUser, deleteUser } = require("../controllers/users.controllers");
 
-router.route("/").get(getAllUsers).post(postUser);
-router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+// router.route("/").get(getAllUsers).post(postUser);
+// router.route("/:id").put(updateUser).delete(deleteUser);
+
+router.get("/", getAllUsers);
+router.get("/find/:id", verifyTokenAndAdmin, getUser);
+router.get("/stats", verifyTokenAndAdmin, getUserStats);
+router.put("/:id", verifyTokenAndAuthorization, updateUser);
+router.delete("/:id", verifyTokenAndAuthorization, deleteUser);
 
 module.exports = router;
